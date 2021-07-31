d218b751dc78ba4aab7eb3d541f9c73206781a87
Paul Baecher <paul.baecher@getyourguide.com>
2021-07-31 19:18:01 +0200

    Add configuration for barium

--- configuration.nix
+++ configuration.nix
@@ -8,96 +8,145 @@ let
   compiledLayout = pkgs.runCommand "keyboard-layout" {} ''
     ${pkgs.xorg.xkbcomp}/bin/xkbcomp -w 0 ${files/keyboard-layout} $out
   '';
+  my-python-packages = python-packages: with python-packages; [
+    pip
+    pipx
+  ];
+  python-with-my-packages = pkgs.python3.withPackages my-python-packages;
 in {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.initrd.luks.devices.root = {
+    device = "/dev/disk/by-uuid/0de8d888-008f-4abd-98fa-7ab2ad56e819";
+    preLVM = true;
+    allowDiscards = true;
+  };
+
+  # temporary workaround for non-working WiFi on 5.10
+  boot.kernelPackages = pkgs.linuxPackages_5_12;
+
+  services.tlp.enable = true;
+
+  services.thinkfan.enable = true;
+
+  networking.hostName = "barium"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  programs.nm-applet.enable = true;
+
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  time.timeZone = "Europe/Berlin";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.autoRepeatDelay = 330;
+  services.xserver.autoRepeatInterval = 25;
+
+  services.xserver.displayManager.defaultSession = "none+i3";
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "pb";
+  services.xserver.displayManager.sessionCommands = "${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledLayout} $DISPLAY";
+
+  services.xserver.libinput.enable = true;
+
+  fonts.fonts = with pkgs; [
+    corefonts
+    iosevka
+  ];
+
+  programs.xss-lock.enable = true;
+  programs.xss-lock.lockerCommand = "${pkgs.i3lock}/bin/i3lock -n -c 202020";
+
+  services.openssh.enable = true;
+  programs.ssh.startAgent = true;
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pb = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" "docker" ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     (neovim.override { vimAlias = true; })
-    (texlive.combine { inherit (texlive) scheme-basic standalone microtype pgf xkeyval xcolor koma-script babel-german; })
-    android-studio
+    alacritty
     bat
     brightnessctl
     caffeine-ng
     clojure
+    docker-compose
     ettercap
     feh
     ffmpeg
     file
     firefox
-    gcc
     git
     gnumake
     gnupg
-    gnuplot
-    go
     httpie
     imagemagick
+    jdk8
+    jdk11
+    jetbrains.idea-ultimate
+    jq
     lshw
-    minetest
     mplayer
     openssl
     pass
     pavucontrol
     pinentry-curses
     pipenv
+    postgresql
     powertop
     pwgen
-    python3
+    python-with-my-packages
     rlwrap
+    sbt
     scrot
+    slack
     spotify
-    termite
-    typespeed
     unzip
     wget
     whois
     xorg.xkbcomp
+    zoom-us
   ];
-
-  virtualisation.docker.enable = true;
-
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  #
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
-
-  services.tlp.enable = true;
-
-  networking.hostName = "caesium"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  programs.nm-applet.enable = true;
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s25.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Redshift.
-  services.redshift.enable = true;
-  services.redshift.temperature.day = 5000;
-  services.redshift.temperature.night = 3000;
-  location.latitude = 52.5;
-  location.longitude = 13.4;
-
-  networking.firewall.allowedTCPPorts = [ 4711 ];
-
-  nixpkgs.config.allowUnfree = true;
 
   environment.variables = {
     EDITOR = "vim";
@@ -112,55 +161,42 @@ in {
     cat = "bat -p";
   };
 
-  fonts.fonts = with pkgs; [
-    corefonts
-    iosevka
-  ];
+  environment.etc = with pkgs; {
+    "libstdcpp".source = "${stdenv.cc.cc.lib}/lib";
+    "libc".source = "${glibc}/lib";
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  programs.ssh.startAgent = true;
-
-  # gpg
-  programs.gnupg.agent.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.autoRepeatDelay = 330;
-  services.xserver.autoRepeatInterval = 25;
-
-  programs.xss-lock.enable = true;
-  programs.xss-lock.lockerCommand = "${pkgs.i3lock}/bin/i3lock -n -c 202020";
-
-  programs.adb.enable = true;
-
-  # Auto login.
-  services.xserver.displayManager.defaultSession = "none+i3";
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "pb";
-  services.xserver.displayManager.sessionCommands = "${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledLayout} $DISPLAY";
-
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pb = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "audio" "video" "docker"];
+    "jdk8".source = jdk8;
+    "jdk11".source = jdk11;
   };
+
+  virtualisation.docker.enable = true;
+
+  services.redshift.enable = true;
+  services.redshift.temperature.day = 5000;
+  services.redshift.temperature.night = 3000;
+  location.latitude = 52.5;
+  location.longitude = 13.4;
+
+  # only want psql client
+  services.postgresql.enable = false;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 22 4711 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -168,6 +204,7 @@ in {
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "21.05"; # Did you read the comment?
 
 }
+
